d7a20d784b496e55cfc4868b0647e1e44507e70f
Anushka16July <87813443+Anushka16July@users.noreply.github.com>
2023-12-03 11:59:30 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+const { v4: uuidv4 } = require("uuid");
+
+const nodeMailer = require("nodemailer");
+const transporter = nodeMailer.createTransport({
+    port: 465,
+    host:"smtp.gmail.com",
+    auth:{
+        user:"anushka24996@gmail.com",
+        pass:"bfvl qijq zgxk qcfu"
+    },
+    secure:true
+})
+
+const io = require("socket.io")(server, {
+    cors: {
+        origin: '*'
+    }
+});
+
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+    debug: true,
+});
+
+app.use("/peerjs", peerServer);
+
+app.get("/", (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get("/:room", (req, res) => {
+    res.render("index", { roomId: req.params.room });
+});
+
+app.post("/send-email", (req, res)=>{
+  const to = req.body.to;
+  const url = req.body.url;
+  const mailData = {
+    from:"anushka24996@gmail.com",
+    to:to,
+    subject:"Join the video chat with me!!",
+    html:`<p>Hi, come and join with me for a video chat</p>`
+  }
+  transporter.sendMail(mailData,(error,info)=>{
+    if(error){
+        return console.log(error)
+    }
+    res.status(200).send({message:"Invitation send", message_id:info.messageId})
+  })
+})
+
+io.on("connection", (socket) => {
+    socket.on("join-room", (roomId, userId, userName) => {
+        socket.join(roomId);
+        io.to(roomId).emit("user-connected", userId);
+        socket.on("message", (message) => {
+            io.to(roomId).emit("createMessage", message, userName);
+        });
+    });
+});
+
+server.listen(process.env.PORT || 3030);
